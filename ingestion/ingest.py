81fba90ac59cb5538ea1f82ed08f3dee57433fa7
Jan S <jansauerland@gmx.de>
2026-08-11 16:21:00 +0200

    Added Logging

--- ingestion/ingest.py
+++ ingestion/ingest.py
@@ -1,9 +1,11 @@
-from dataclasses import dataclass
 from pathlib import Path
 import os
 from dotenv import load_dotenv
 import pandas as pd
 import sqlalchemy
+import logging
+import sys
+from logging.handlers import RotatingFileHandler
 
 load_dotenv()
 
@@ -12,8 +14,39 @@ DB_DEFINITION = Path('table_definition.xml')
 ENCODING = os.getenv("ENCODING", "utf-8")
 
 
+class StreamFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelno in [logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL]:
+            return True
+        else:
+            return False
+
+
+def set_logger(log_name: str, log_level: str = "INFO", log_path: str = "./logs"):
+    logger = logging.getLogger(log_name)
+    logger.setLevel(getattr(logging, log_level.upper()))
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    log_file = os.path.join(log_path, f"{log_name}.log")
+    file_handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=256, encoding=ENCODING)
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_formatter = logging.Formatter('[%(name)s] %(asctime)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(stream_formatter)
+    stream_handler.addFilter(StreamFilter())
+    logger.addHandler(stream_handler)
+    return logger
+
+
+LOGGER = set_logger(os.getenv("LOGGER_NAME", "replay"), os.getenv("LOGGER_LEVEL", "INFO"),
+                    os.getenv("LOGGER_PATH", "./logs"))
+
+
 def start_ingestion(file_path: str) -> dict:
     # load the csv files into Tables
+    LOGGER.info(f'Loading CSV files from path: {file_path}')
     games_table = _load_csv_into_table(Path(file_path, "games.csv"), ENCODING)
     events_table = _load_csv_into_table(Path(file_path, "events.csv"), ENCODING)
     pitches_table = _load_csv_into_table(Path(file_path, "pitches.csv"), ENCODING)
@@ -22,39 +55,40 @@ def start_ingestion(file_path: str) -> dict:
     sql_string = os.getenv("POSTGRES_DB_CONNECT_STRING")
     if sql_string:
         sql_engine = sqlalchemy.create_engine(sql_string)
+        LOGGER.info(f'Connected to database with connection string: {sql_string}')
     else:
+        LOGGER.critical("No database connection string provided in environment variables.")
         raise Exception("No database connection string provided in environment variables.")
 
-    games_rows = games_table.to_sql('games', sql_engine, if_exists='replace', index=False)
-    events_rows = events_table.to_sql('events', sql_engine, if_exists='replace', index=False)
-    pitches_rows = pitches_table.to_sql('pitches', sql_engine, if_exists='replace', index=False)
+    LOGGER.info('Saving tables to database.')
+    games_table.to_sql('games', sql_engine, if_exists='replace', index=False)
+    events_table.to_sql('events', sql_engine, if_exists='replace', index=False)
+    pitches_table.to_sql('pitches', sql_engine, if_exists='replace', index=False)
 
+    LOGGER.info(f'Successfully saved tables to database: games, events, pitches')
     return {
-        "games": games_rows,
-        "events": events_rows,
-        "pitches": pitches_rows
+        "games": len(games_table),
+        "events": len(events_table),
+        "pitches": len(pitches_table)
     }
-
-
-@dataclass(frozen=True)
-class PdTable:
-    table_name: str
-    df: pd.DataFrame
-    # TODO: remove all usages of this, then delete
 
 
 def _load_csv_into_table(file_path: Path, encoding: str = "utf-8") -> pd.DataFrame:
     if not file_path.exists():
+        LOGGER.critical("CSV file does not exist.")
         raise FileNotFoundError(f"CSV file does not exist: {file_path}")
     if not DB_DEFINITION.exists():
+        LOGGER.critical("Database definition file does not exist.")
         raise FileNotFoundError(f"Database definition file does not exist: {DB_DEFINITION}")
 
-    return pd.read_csv(
+    LOGGER.info(f'Loading CSV file: {file_path}')
+    df = pd.read_csv(
         file_path,
         encoding=encoding,
         na_values=["--"],
-        keep_default_na=True,
-    )
+        keep_default_na=True)
+    LOGGER.info(f'Successfully loaded CSV file: {file_path}')
+    return df
 
 
 if __name__ == '__main__':
--- replay/replay.py
+++ replay/replay.py
@@ -1,14 +1,46 @@
 import sqlalchemy
-
-from ingestion import ingest
 import pandas as pd
 from kafka import KafkaProducer
 import json
 from dotenv import load_dotenv
 import os
 import time
+import logging
+import sys
+from logging.handlers import RotatingFileHandler
 
 load_dotenv()
+
+ENCODING = os.getenv("ENCODING", "utf-8")
+
+class StreamFilter(logging.Filter):
+    def filter(self, record):
+        if record.levelno in [logging.INFO, logging.WARNING, logging.ERROR, logging.CRITICAL]:
+            return True
+        else:
+            return False
+
+
+def set_logger(log_name: str, log_level: str = "INFO", log_path: str = "./logs"):
+    logger = logging.getLogger(log_name)
+    logger.setLevel(getattr(logging, log_level.upper()))
+    if not os.path.exists(log_path):
+        os.makedirs(log_path)
+    log_file = os.path.join(log_path, f"{log_name}.log")
+    file_handler = RotatingFileHandler(log_file, maxBytes=1024 * 1024, backupCount=256, encoding=ENCODING)
+    file_formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+    file_handler.setFormatter(file_formatter)
+    logger.addHandler(file_handler)
+    stream_handler = logging.StreamHandler(sys.stdout)
+    stream_formatter = logging.Formatter('[%(name)s] %(asctime)s - %(levelname)s - %(message)s')
+    stream_handler.setFormatter(stream_formatter)
+    stream_handler.addFilter(StreamFilter())
+    logger.addHandler(stream_handler)
+    return logger
+
+
+LOGGER = set_logger(os.getenv("LOGGER_NAME", "replay"), os.getenv("LOGGER_LEVEL", "INFO"),
+                    os.getenv("LOGGER_PATH", "./logs"))
 
 
 def set_producer(server='localhost', port=9092):
@@ -19,6 +51,7 @@ def set_producer(server='localhost', port=9092):
 
 def replay_kafka():
     replay_delay = float(os.getenv('KAFKA_REPLAY_DELAY', '0.5'))  # delay in seconds between events
+    LOGGER.info(f'Starting replay to Kafka with delay of {replay_delay} seconds between events.')
 
     producer = set_producer(server=os.getenv('KAFKA_HOST', 'localhost'),
                             port=int(os.getenv('KAFKA_PORT', 9092)))
@@ -26,21 +59,21 @@ def replay_kafka():
     sql_string = os.getenv("POSTGRES_DB_CONNECT_STRING")
     if sql_string:
         sql_engine = sqlalchemy.create_engine(sql_string)
+        LOGGER.info(f'Connected to database with connection string: {sql_string}')
     else:
+        LOGGER.critical("No database connection string provided in environment variables.")
         raise Exception("No database connection string provided in environment variables.")
-    tables = _get_tables(sql_engine)
+    games, events, pitches = _get_tables(sql_engine)
 
-    # kafka_key = 0  # internal replay key to secure right order
-    for event in replay(tables):
+    # TODO: Start multiple replays after each other, but with different game-ids, so that we can simulate multiple games at the same time
+    for event in replay(games, events, pitches):
         future = producer.send(topic=os.getenv('KAFKA_TOPIC', 'game-events'), key=event["game-id"], value=event)
         result = future.get(timeout=10)  # wait for response, so it's synchronous processing
-        print(f'Message sent for event: {event}')
-        # kafka_key += 1
+        LOGGER.info(f'Message sent for event: {event}')
         time.sleep(replay_delay)
 
 
-def _get_tables(sql_in) -> list[ingest.PdTable]:
-    tables = []
+def _get_tables(sql_in):
     games_df = pd.read_sql_query('SELECT * FROM public.games LIMIT 2', sql_in)  # TODO: remove LIMIT 2 in production
     # read the events that fit the game ids
     game_ids = games_df["Game"].tolist()
@@ -48,51 +81,30 @@ def _get_tables(sql_in) -> list[ingest.PdTable]:
                                   sql_in)
     pitches_df = pd.read_sql_query(
         f'SELECT * FROM public.pitches WHERE "Game" IN ({",".join(map(str, game_ids))})', sql_in)
-    print('SqlIn - All Tables loaded from db')
+    LOGGER.info(f'Loaded tables from database: games ({len(games_df)} rows), events ({len(events_df)} rows), pitches ({len(pitches_df)} rows)')
 
-    games_table = ingest.PdTable(table_name='games', df=games_df)
-    events_table = ingest.PdTable(table_name='events', df=events_df)
-    pitches_table = ingest.PdTable(table_name='pitches', df=pitches_df)
-    tables.extend([games_table, events_table, pitches_table])
-    return tables
+    return games_df, events_df, pitches_df
 
 
-def replay(tables: list[ingest.PdTable]):
-    # replay mode is always pitch by pitch
-    table_games: pd.DataFrame | None = None
-    table_events: pd.DataFrame | None = None
-    table_pitches: pd.DataFrame | None = None
-
-    for table in tables:
-        match table.table_name:
-            case "games":
-                table_games = table.df
-                # static: game-id, away, home, stadium, date, location, umpires
-                # dynamic: away-record, home-record, away-score, home-score, stats...
-            case "events":
-                table_events = table.df
-                # game-id, pitching-team, batting-team, inning, event-id, events, away, home
-            case "pitches":
-                table_pitches = table.df
-                # Num(per batter), pitch, type, mph, hitzone, play-bases, play-field, Pitcher, pitching-team, batting-team, inning, event-id, game-id
+def replay(table_games: pd.DataFrame, table_events: pd.DataFrame, table_pitches: pd.DataFrame):
+    # table_games
+    # static: game-id, away, home, stadium, date, location, umpires
+    # dynamic: away-record, home-record, away-score, home-score, stats...
+    # table_events
+    # game-id, pitching-team, batting-team, inning, event-id, events, away, home
+    # table_pitches
+    # Num(per batter), pitch, type, mph, hitzone, play-bases, play-field, Pitcher, pitching-team, batting-team, inning, event-id, game-id
 
     # event definition
     # event-type: pitch | event | inning-start | inning-end | game-start | game-end
-    # easy mode: anonymous batters
     # new game -> game-start with static info (game-id!)
     # inning-start -> first event in events with new half-inning
     # pitch -> pitches of events=event-id in num order
     # event -> event from end of pitches for this event (ignore events with empty teams - position changes)
     # inning-end -> after last event in events ending half-inning
+    # game-end -> after last event in events for this game
 
     # iterate through pandas dataframe of table_games and process games data sequentially
-    if table_games is None:
-        raise ValueError("Missing required table: games")
-    elif table_events is None:
-        raise ValueError("Missing required table: events")
-    elif table_pitches is None:
-        raise ValueError("Missing required table: pitches")
-
     for _, game in table_games.iterrows():
         game_id = game.get("Game")
         game_events = table_events[table_events["Game"] == game_id]
@@ -149,6 +161,7 @@ def replay(tables: list[ingest.PdTable]):
                     "pitch-type": pitch.get("Type"),
                     "pitch-speed": pitch.get("MPH"),
                     "pitch-location": pitch.get("play-hitzone"),
+                    "play-bases": pitch.get("play-bases"),
                 }
                 yield current_event
 
